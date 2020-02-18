0e9fa8221f580588471efc081d8b2d64c6cfd85b
Whiskarek <andrew.shkrob.social@yandex.by>
2020-02-18 16:27:18 +0300

    finished chat message class

--- Client/chat_message.hpp
+++ Client/chat_message.hpp
@@ -5,104 +5,58 @@
 #include <cstring>
 #include <ctime>
 
-class ChatMessage;
-
 enum class MessageType {
-    kOnlineState = 0,
-    kSimpleMessage,
-    kUnknown = 100,
+    CONNECT = 0,
+    COMMAND = 1,
+    MESSAGE = 2,
+    UNKNOWN = 3
 };
 
 class ChatMessage {
 public:
+    static constexpr size_t MESSAGE_SIZE = 1025;
 
-    static constexpr size_t kMessageSize = 1025;
+    ChatMessage() : _type(MessageType::UNKNOWN) {}
 
-    ChatMessage() : data_(), type_(MessageType::kUnknown) {
-        data_[0] = static_cast<char>(MessageType::kUnknown);
+    explicit ChatMessage(const std::string &text) {
+        if (text.empty())
+            _type = MessageType::UNKNOWN;
+        else if (text.starts_with("/connect"))
+            _type = MessageType::CONNECT;
+        else if (text.starts_with('/'))
+            _type = MessageType::COMMAND;
+        else
+            _type = MessageType::MESSAGE;
+        _data[0] = static_cast<char>(_type);
+        memcpy(_data.data(), text.data(), sizeof(char) * std::min(MESSAGE_SIZE - 1, text.size()));
     }
 
-    explicit ChatMessage(std::time_t time) : data_(), type_(MessageType::kOnlineState) {
-        WriteToMessage(time);
+    void decode() {
+        _type = static_cast<MessageType>(_data[0]);
     }
 
-    explicit ChatMessage(char data[1025]) : data_(), type_(MessageType::kUnknown) {
-        data_[0] = static_cast<char>(MessageType::kUnknown);
-        memcpy(data_.data(), data, sizeof(char) * 1025);
-        Decode();
+    [[nodiscard]] MessageType get_type() const {
+        return _type;
     }
 
-    explicit ChatMessage(const std::string &textMessage) : data_(), type_(MessageType::kSimpleMessage) {
-        data_[0] = static_cast<char>(MessageType::kSimpleMessage);
-        for (size_t i = 0; i < std::max(kMessageSize - 1, textMessage.size()); ++i) {
-            data_[i + 1] = textMessage[i];
-        }
+    boost::array<char, MESSAGE_SIZE> &get_raw_data() {
+        return _data;
     }
 
-    void Decode() {
-        switch (static_cast<char>(data_[0])) {
-            case 0: {
-                type_ = MessageType::kOnlineState;
-                return;
-            }
-            case 1: {
-                type_ = MessageType::kSimpleMessage;
-                return;
-            }
-            default: {
-                type_ = MessageType::kUnknown;
-                return;
-            }
-        }
-    }
-
-    [[nodiscard]] MessageType GetType() const {
-        return type_;
-    }
-
-    boost::array<char, kMessageSize> &GetRawData() {
-        return data_;
-    }
-
-    template<typename T>
-    void WriteToMessage(const T &data) {
-        const char *begin = reinterpret_cast<const char *>(std::addressof(data));
-        const char *end = begin + sizeof(T);
-        std::copy(begin, end, data_.begin() + 1);
-    }
-
-    void WriteToMessage(const std::string &data) {
-        type_ = MessageType::kSimpleMessage;
-        data_[0] = static_cast<char>(MessageType::kSimpleMessage);
-        for (size_t i = 0; i < std::max(kMessageSize - 1, data.size()); ++i) {
-            data_[i + 1] = data[i];
-        }
-    }
-
-    template<typename T>
-    T ReadFromMessage() const {
-        T ret;
-        char *begin = reinterpret_cast<char *>(std::addressof(ret));
-        std::copy(data_.begin() + 1, data_.begin() + 1 + sizeof(T), begin);
-        return ret;
-    }
-
-    [[nodiscard]] std::string ReadText() const {
-        if (type_ != MessageType::kSimpleMessage) {
+    [[nodiscard]] std::string get_text() const {
+        if (_type == MessageType::UNKNOWN) {
             throw;
         }
-        std::string ret{};
-        for (size_t i = 1; i < kMessageSize && data_[i] != 0; ++i) {
-            ret.push_back(data_[i]);
+        std::string ret;
+        for (size_t i = 1; i < MESSAGE_SIZE && _data[i] != 0; ++i) {
+            ret.push_back(_data[i]);
         }
         return ret;
     }
 
-
 private:
-    MessageType type_;
-    boost::array<char, kMessageSize> data_;
-
+    MessageType _type;
+    boost::array<char, MESSAGE_SIZE> _data;
 };
 
 #endif //CLIENT_CHAT_MESSAGE_HPP
